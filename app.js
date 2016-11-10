8b663046d28053c85da91bc8ea8f8f5554706d05
lenni <lenni31@neuf.fr>
2016-11-10 14:07:14 +0100

    modif css commencement

--- app.js
+++ app.js
@@ -1,1 +1,41 @@
-console.log("teste");
+(function(){
+	"use strict";
+	var app = {
+
+		red: 
+
+		init:function(){
+			app.listerners();
+		},
+
+		listerners: function(){
+			$('#R').on('input change', app.red);
+			$('#G').on('input change', app.green);
+			$('#B').on('input change', app.blue);
+			$('#A').on('input change', app.alpha);
+		},
+
+		red: function(){
+			var r = $('#R').val();
+			
+		},
+
+		green: function(){
+			console.log('teste');
+		},
+
+		blue: function(){
+			console.log('teste');
+
+		},
+
+		alpha: function(){
+			
+		},
+	};
+
+
+	$(document).ready(function(){
+		app.init();
+	});
+})();
